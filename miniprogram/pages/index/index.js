a39069af17976d47cdb7a4240d9761079ca9cea1
sharon <403431641@qq.com>
2018-10-02 16:10:47 +0800

    iconfont&indeximages

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,9 +7,18 @@ Page({
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: '',
+    movies: [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ] 
   },
   
+  // onLoad: function () {
+  // },  
+
   Menu(){
     wx.navigateTo({
       url: '../Menu/Menu',
@@ -31,3 +40,4 @@ Page({
     })
   }
 })
+
